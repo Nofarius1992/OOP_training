d87fe1040b427673b518ec37a58c19e68d2a1574
Nofarius1992 <tolya.prozhoga@gmail.com>
2020-11-06 17:04:47 +0200

    2

--- index.php
+++ index.php
@@ -1,2 +1,23 @@
 <?php
-    echo 'test';
+
+	class Student {
+		public $name;
+		public $course;
+
+		public function transferToNextCourse($course)
+		{
+			
+		}
+
+		private function isCourseCorrect($course) {
+			if($course < 5) {
+				return $this->course = $course +1;
+			}
+		}
+	}
+
+	$student = new Student();
+	$student->transferToNextCourse(2);
+	echo $student->course;
+
+
